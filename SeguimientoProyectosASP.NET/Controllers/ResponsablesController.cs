6e0dc43ac100f671be07605308daacb97f6e19bc
Titiushko <titotauro@gmail.com>
2015-10-05 18:04:21 -0600

    Generando CRUD para Responsables, Tareas y Usuarios.

---
+++ SeguimientoProyectosASP.NET/Controllers/ResponsablesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeguimientoProyectosASP.NET.Models;
+
+namespace SeguimientoProyectosASP.NET.Controllers
+{
+    public class ResponsablesController : Controller
+    {
+        private SeguimientoProyectosEntities db = new SeguimientoProyectosEntities();
+
+        // GET: Responsables
+        public ActionResult Index()
+        {
+            var responsable = db.Responsable.Include(r => r.Tarea).Include(r => r.Usuario);
+            return View(responsable.ToList());
+        }
+
+        // GET: Responsables/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(responsable);
+        }
+
+        // GET: Responsables/Create
+        public ActionResult Create()
+        {
+            ViewBag.codigo_tarea = new SelectList(db.Tarea, "codigo", "nombre");
+            ViewBag.codigo_usuario = new SelectList(db.Usuario, "codigo", "nombres");
+            return View();
+        }
+
+        // POST: Responsables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codigo,codigo_tarea,codigo_usuario")] Responsable responsable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Responsable.Add(responsable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.codigo_tarea = new SelectList(db.Tarea, "codigo", "nombre", responsable.codigo_tarea);
+            ViewBag.codigo_usuario = new SelectList(db.Usuario, "codigo", "nombres", responsable.codigo_usuario);
+            return View(responsable);
+        }
+
+        // GET: Responsables/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.codigo_tarea = new SelectList(db.Tarea, "codigo", "nombre", responsable.codigo_tarea);
+            ViewBag.codigo_usuario = new SelectList(db.Usuario, "codigo", "nombres", responsable.codigo_usuario);
+            return View(responsable);
+        }
+
+        // POST: Responsables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codigo,codigo_tarea,codigo_usuario")] Responsable responsable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(responsable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.codigo_tarea = new SelectList(db.Tarea, "codigo", "nombre", responsable.codigo_tarea);
+            ViewBag.codigo_usuario = new SelectList(db.Usuario, "codigo", "nombres", responsable.codigo_usuario);
+            return View(responsable);
+        }
+
+        // GET: Responsables/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(responsable);
+        }
+
+        // POST: Responsables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            Responsable responsable = db.Responsable.Find(id);
+            db.Responsable.Remove(responsable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SeguimientoProyectosASP.NET/Controllers/TareasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeguimientoProyectosASP.NET.Models;
+
+namespace SeguimientoProyectosASP.NET.Controllers
+{
+    public class TareasController : Controller
+    {
+        private SeguimientoProyectosEntities db = new SeguimientoProyectosEntities();
+
+        // GET: Tareas
+        public ActionResult Index()
+        {
+            var tarea = db.Tarea.Include(t => t.Proyecto);
+            return View(tarea.ToList());
+        }
+
+        // GET: Tareas/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // GET: Tareas/Create
+        public ActionResult Create()
+        {
+            ViewBag.codigo_proyecto = new SelectList(db.Proyecto, "codigo", "nombre");
+            return View();
+        }
+
+        // POST: Tareas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codigo,nombre,descripcion,codigo_proyecto")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarea.Add(tarea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.codigo_proyecto = new SelectList(db.Proyecto, "codigo", "nombre", tarea.codigo_proyecto);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.codigo_proyecto = new SelectList(db.Proyecto, "codigo", "nombre", tarea.codigo_proyecto);
+            return View(tarea);
+        }
+
+        // POST: Tareas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codigo,nombre,descripcion,codigo_proyecto")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.codigo_proyecto = new SelectList(db.Proyecto, "codigo", "nombre", tarea.codigo_proyecto);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            Tarea tarea = db.Tarea.Find(id);
+            db.Tarea.Remove(tarea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
